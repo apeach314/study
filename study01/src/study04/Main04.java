70a9e7c5dec0efbb08bbd91523684b050bfd539f
Kim Jong Feel <DeveloperFEEL@gmail.com>
2018-09-15 00:48:02 +0900

    study 04 code update: stream api, meeting record

--- study01/src/study04/Main04.java
+++ study01/src/study04/Main04.java
@@ -1,6 +1,7 @@
 package study04;
 
 import java.util.Arrays;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 
 public class Main04 {
@@ -15,13 +16,16 @@ public class Main04 {
         String[] strNX = nx.split(" ");
         int[] arrayNX = Arrays.stream(strNX).mapToInt(Integer::parseInt).toArray();
         String[] strA = str.split(" ");
-        int[] arrayA = Arrays.stream(strA).mapToInt(Integer::parseInt).toArray();
+        int[] arrayA = Arrays.stream(strA).mapToInt(Integer::parseInt).filter(n -> n < arrayNX[1]).toArray();
         
-        for(int i=0; i<arrayNX[0]; i++ ){
-            if(arrayA[i]<arrayNX[1]){
-                 System.out.print(arrayA[i]+" ");
-            }
-        }
+        for (int i : arrayA) {
+        	System.out.print(i+" ");
+		}
+//        for(int i=0; i<arrayNX[0]; i++ ){
+//            if(arrayA[i]<arrayNX[1]){
+//                 System.out.print(arrayA[i]+" ");
+//            }
+//        }
 	}
 	
 	public static void Baekjoon1546() {
@@ -48,13 +52,35 @@ public class Main04 {
 	   	 int c = Integer.parseInt(scanner.nextLine());
 	   	 String str ;
 	   	 String[] strA;
-	   	 double[] scoreA;
-	   	 double sum, avg,n, x;
+	   	 // 저장을 위해서는 jagged array가 필요
+	   	 double[][] scoreA = new double[c][];
+	   	 OptionalDouble[] avg = new OptionalDouble[c];
+	   	 long lessThanAverageCount[] = new long[c];
 	   	 
-	   	 for(int i=0; i<c; i++){
+	   	 for(int i=0; i<c; i++) {
 	   		 str = scanner.nextLine();
 	   		 strA = str.split(" ");
-	   		 scoreA = Arrays.stream(strA).mapToDouble(Double::parseDouble).toArray();
+	   		scoreA[i] = Arrays.stream(strA).mapToDouble(Double::parseDouble).toArray();
+	   		int length = scoreA[i].length;
+	   		
+	   		 avg[i] = Arrays.stream(strA, 1, length).mapToDouble(Double::parseDouble).average();
+	   		//System.out.println(avg[i]);
+	   		
+	   		OptionalDouble average = avg[i];
+	   		lessThanAverageCount[i] = Arrays.stream(strA, 1, length).mapToInt(Integer::parseInt).filter(num -> num > average.getAsDouble()).count();
+	   		//System.out.println(lessThanAverageCount[i]);
+	   		
+	   		//System.out.format("%.3f%n", lessThanAverageCount[i] / (double)(length - 1) * 100);
+	   	 }
+	   	 
+	   	 for (int i=0; i<c; i++) {
+	   		 int length = scoreA[i].length;
+	   		 // length - 1 인 이유: index 0 은 성적이 아니고 성적의 갯수를 입력하는 칸, 그러므로 length -1이 필요
+	   		 System.out.format("%.3f%n", lessThanAverageCount[i] / (double)(length - 1) * 100);
+	   	 }
+	   	 
+	   	 /*
+	   	  * scoreA = Arrays.stream(strA).mapToDouble(Double::parseDouble).toArray();
 	   		 n = scoreA[0]; avg=0; sum=0; x=0;
 	   		 
 	   		 for(int j=1;j<scoreA.length; j++){
@@ -68,14 +94,14 @@ public class Main04 {
 	   			 }
 	   		 }
 	   		 System.out.format("%.3f%n",x/n*100);
-	   	 }
+	   	  */
 	   	 
 	   	 scanner.close();
 	}
 	
      public static void main(String[] args) {
-    	 Baekjoon10871();
+    	 //Baekjoon10871();
     	 //Baekjoon1546();
-    	 //Baekjoon4344();
+    	 Baekjoon4344();
      }
 }
