1fa0f4b840106b6794bcfb9617f72ee8a2e43009
esjung <apeach314@gmail.com>
2018-11-23 23:19:44 +0900

    ㅠㅠ

--- study01/src/study07/Main07.java
+++ study01/src/study07/Main07.java
@@ -12,29 +12,29 @@ public class Main07 {
 		
 		int cnt = 0;
 		
-		if (n==1) {
-			System.out.println(cnt);
-		} else {
-			// 소수는 -1 부터 해야하는 것 같음
+//		if (n==1) {
+//			System.out.println(cnt);
+//		} else {
 			for (int i=2; i<=n; i++) {
-				// 소수일때
-		        if (n / i != 0) {
-		        	n = n-1;
-		        	cnt = cnt + 1;	// 연산횟수를 센다
-		        	/*if (n%3==0) {
-		        		
-		        		System.out.println("3으로 나눈 나머지가 0 .. "+cnt);
-		        	} else if (n%2==0) {
-		        		
-		        		System.out.println("2로 나눈 나머지가 0 .. "+cnt);
-		        	} else {
-		        		System.out.println("n이 2 인가 .. "+cnt);
-		        	}*/
-		        	break;
-		        }
+    			
+    			if (n%3==0) {
+	        		n = n/3;
+	        		cnt = cnt + 1;
+	        	} else if (n%2==0) {
+	        		n = n/2;
+	        		cnt = cnt + 1;
+	        		System.out.println("2로 나누는 조건문 "+n);
+	        	} else {
+	        		n = n-1;
+	        		cnt = cnt + 1;
+	        	}
+    			
 		    }
-		}
+			System.out.println(cnt);
+//		}
 
+		
+		
 	}
 
 }
