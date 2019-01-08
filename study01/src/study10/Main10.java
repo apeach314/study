b8a6febca1c63c3b20859a396216e283a22d1fb8
esjung <apeach314@gmail.com>
2019-01-08 21:32:21 +0900

    아직..

---
+++ study01/src/study10/Main10.java
@@ -0,0 +1,21 @@
+package study10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main10 {
+	
+	public static void main(String[] args) {
+		Baekjoon1316();
+    }
+		
+	public static void Baekjoon1316() {
+			
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		
+	}
+	
+	
+}
