4c050ad8ddf1507d52331d5c4f156383912dc1b4
esjung <apeach314@gmail.com>
2019-01-10 23:25:39 +0900

    틀린문제?

--- study01/src/study10/Main10.java
+++ study01/src/study10/Main10.java
@@ -13,9 +13,31 @@ public class Main10 {
 			
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
+		String[] cases = new String[n];
+		for ( int i=0; i<n; i++ ) {
+			cases[i] = scanner.next();
+		}
+		scanner.close();
 		
+		int cnt = n;	// 그룹단어 count
+		
+		for(int i=0; i<n; i++) {
+			String str = cases[i];		// 입력받은 단어 
+			int[] array = new int[26];
+			Arrays.fill(array, -1);		// 전부 -1로 세팅.
+			
+			for(int j=1; j<str.length(); j++) {
+				// 한번 나왔던 알파벳인 경우 단어의 앞,뒤 알파벳을 비교해서 다르면 그룹단어 카운트 안함.
+				if(array[str.charAt(j)-'a']==0 && str.charAt(j)!=str.charAt(j-1) ) {
+					array[str.charAt(j)-'a']=-1;		// 알파벳 체크
+					cnt--;
+				}else{
+					array[str.charAt(j)-'a']=0;
+				}
+			}
+		}
+		System.out.println(cnt);
 		
 	}
-	
 	
 }
