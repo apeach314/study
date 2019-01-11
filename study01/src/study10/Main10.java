29629bf3cef985f5caa576a8becb7a6e286dbcbb
Kim Jong Feel <DeveloperFEEL@gmail.com>
2019-01-12 00:48:47 +0900

    Fix error

--- study01/src/study10/Main10.java
+++ study01/src/study10/Main10.java
@@ -26,11 +26,17 @@ public class Main10 {
 			int[] array = new int[26];
 			Arrays.fill(array, -1);		// 전부 -1로 세팅.
 			
+			if (str.length() == 1) {
+				continue;
+			}
+			
+			array[str.charAt(0)-'a'] = 0;
 			for(int j=1; j<str.length(); j++) {
 				// 한번 나왔던 알파벳인 경우 단어의 앞,뒤 알파벳을 비교해서 다르면 그룹단어 카운트 안함.
 				if(array[str.charAt(j)-'a']==0 && str.charAt(j)!=str.charAt(j-1) ) {
 					array[str.charAt(j)-'a']=-1;		// 알파벳 체크
 					cnt--;
+					break;
 				}else{
 					array[str.charAt(j)-'a']=0;
 				}
